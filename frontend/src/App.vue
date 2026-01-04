2e5fa1b72602e47a7a41092b11870b7b57be1a0c
Rodion <ergerplus@gmail.com>
2026-01-04 21:36:08 +0100

    1

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,32 @@
 <template>
   <div id="app">
-    <h1>Arbitration V1</h1>
-    <p>Start param: {{ startParam }}</p>
+    <div id="header">
+      <div id="online">Offline</div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { onMounted } from 'vue'
 import WebApp from "@twa-dev/sdk"
-
-const startParam = ref('')
 
 onMounted(() => {
   WebApp.ready()
   WebApp.expand()
-  startParam.value = WebApp.initDataUnsafe.start_param || 'none'
   console.log('start_param:', WebApp.initDataUnsafe.start_param)
 })
 </script>
 
 <style>
+#header {
+  display: flex;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
