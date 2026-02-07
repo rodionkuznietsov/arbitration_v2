6fcf15439b3db00acf358bde854792967fcd0c88
Rodion <ergerplus@gmail.com>
2026-02-07 17:24:48 +0100

    add 5m interval

--- frontend/src/components/AppMenu.vue
+++ frontend/src/components/AppMenu.vue
@@ -33,9 +33,7 @@
         background-color: rgba(48, 60, 81, 0.404);
         left: 0;
         right: 0;
-        margin: 0 auto;
-        margin-left: 125px;
-        margin-right: 125px;
+        margin: 0 100px;
         border-radius: 30px;
         font-size: 16px;
         z-index: 1;
