1c371701a6e0bb47e203f94a0949c0f943bff1e6
Rodion <ergerplus@gmail.com>
2026-04-01 12:47:43 +0200

    update menu

--- frontend/src/components/AppMenu.vue
+++ frontend/src/components/AppMenu.vue
@@ -50,7 +50,7 @@
         justify-content: center;
         width: 100%;
         background-color: var(--basic-Bg);
-        margin: 0 60px;
+        margin: 0 50px;
         border-radius: 15px;
     }
 
