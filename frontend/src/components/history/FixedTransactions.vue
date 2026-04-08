74bdf7d5634e5f6e2f1ba6f16f9017cf7b954324
Rodion <ergerplus@gmail.com>
2026-04-08 19:36:48 +0200

    update

--- frontend/src/components/history/FixedTransactions.vue
+++ frontend/src/components/history/FixedTransactions.vue
@@ -1,21 +1,23 @@
 <template>
-    <div class="title">Активные сделки</div>
-    <table class="active-order">
-        <thead>
-            <td>#</td>
-            <td>Статус</td>
-            <td>Биржи</td>
-            <td>Тикер</td>
-            <td>Время работы</td>
-        </thead>
-        <tbody>
-            <td>1</td>
-            <td id="status"><div class="online" id="status_circle"></div>Активный</td>
-            <td>Bybit/Short</td>
-            <td>BTCUSDT</td>
-            <td>19 Минут</td>
-        </tbody>
-    </table>
+    <div style="margin-bottom: 10px;">
+        <div class="title">Активные сделки</div>
+        <table class="active-order">
+            <thead>
+                <td>#</td>
+                <td>Статус</td>
+                <td>Биржи</td>
+                <td>Тикер</td>
+                <td>Время работы</td>
+            </thead>
+            <tbody>
+                <td>1</td>
+                <td id="status"><div class="online" id="status_circle"></div>Активный</td>
+                <td>Bybit/Short</td>
+                <td>BTCUSDT</td>
+                <td>19 Минут</td>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <style scoped>
