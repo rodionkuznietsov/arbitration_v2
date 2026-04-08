09a72eb4f921271ab6e8d8119b8f9ce00cf331ef
Rodion <ergerplus@gmail.com>
2026-04-08 22:58:17 +0200

    update

--- backend/user_api/src/routers/events.py
+++ backend/user_api/src/routers/events.py
@@ -46,12 +46,11 @@ async def subscribe_events(tg_user_id: int):
 
 async def push_to_subscribes(event_data, tg_user_id: None):
     if tg_user_id is not None:
-        for queues in subscribes[tg_user_id]:
-            for queue in queues:
-                try:
-                    queue.put_nowait(event_data)
-                except asyncio.QueueFull:
-                    pass  
+        for queue in subscribes[tg_user_id]:
+            try:
+                queue.put_nowait(event_data)
+            except asyncio.QueueFull:
+                pass  
     else:
         for queues in subscribes.values():
             for queue in queues:
