41b467519a5db55cd76373f4e0a404368f03173f
Rodion <ergerplus@gmail.com>
2026-04-08 17:39:40 +0200

    update

--- backend/user_api/main.py
+++ backend/user_api/main.py
@@ -27,7 +27,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://api.arbitrage-bot.xyz"],
+    allow_origins=["https://arbitrage-bot.xyz"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
