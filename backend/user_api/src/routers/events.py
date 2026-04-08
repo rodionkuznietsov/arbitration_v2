22dfac94567d0eebc68fbd2246bb876c86d962b9
Rodion <ergerplus@gmail.com>
2026-04-08 16:33:49 +0200

    update

--- backend/user_api/src/routers/events.py
+++ backend/user_api/src/routers/events.py
@@ -4,9 +4,13 @@ import json
 from fastapi import APIRouter
 from fastapi.responses import StreamingResponse
 
+import structlog
+
 from ..cache import event_deque
 
 router = APIRouter()
+
+log: structlog.PrintLogger = structlog.get_logger()
 
 async def event_streamer(tg_user_id):
     global event_deque
@@ -16,9 +20,10 @@ async def event_streamer(tg_user_id):
             event = await event_deque.get()
             if event["tg_user_id"] == tg_user_id or event["tg_user_id"] == "all":
                 yield f"data: { json.dumps(event) }\n\n"
-        except asyncio.TimeoutError:
+        except asyncio.TimeoutError as e:
+            log.error(f"EventSender: {e}")
             yield f"data: keep-alive\n\n"
-
+ 
 @router.get("/subscribe/events/{tg_user_id}", tags=["events"])
 async def subscribe_events(tg_user_id: int):
     return StreamingResponse(event_streamer(tg_user_id), media_type="text/event-stream")
