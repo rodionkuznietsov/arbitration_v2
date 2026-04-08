59b83496081af2aa8168e65cb9eb4e1a15fab422
Rodion <ergerplus@gmail.com>
2026-04-08 21:22:16 +0200

    update

--- backend/user_api/src/routers/events.py
+++ backend/user_api/src/routers/events.py
@@ -6,29 +6,37 @@ from fastapi.responses import StreamingResponse
 
 import structlog
 
+from backend.user_api.src import tg_bot_app
+
 from ..cache import event_deque
 
 router = APIRouter()
 
 log: structlog.PrintLogger = structlog.get_logger()
 
-async def event_streamer(tg_user_id: int):
+subscribes_users_id = []
+
+async def event_streamer():
     global event_deque
 
     while True:
         try:
             event = await event_deque.get()
-            if event["tg_user_id"] == tg_user_id or event["tg_user_id"] == "all":
-                log.info(f"User: {event["tg_user_id"]} принял данные")
-                yield f"data: { json.dumps(event) }\n\n"
+            for user_id in subscribes_users_id:
+                if event["tg_user_id"] == user_id or event["tg_user_id"] == "all":
+                    log.info(f"User: {event["tg_user_id"]} принял данные")
+                    yield f"data: { json.dumps(event) }\n\n"
         except asyncio.TimeoutError as e:
             log.error(f"EventSender: {e}")
             yield f"data: keep-alive\n\n"
  
 @router.get("/subscribe/events/{tg_user_id}", tags=["events"])
 async def subscribe_events(tg_user_id: int):
+    if tg_user_id not in subscribes_users_id:
+        subscribes_users_id.append(tg_user_id)
+    
     return StreamingResponse(
-        event_streamer(tg_user_id), 
+        event_streamer(), 
         media_type="text/event-stream",
         headers={"Cache-Control": "no-cache", "X-Accel-Buffering": "no"}
     )
