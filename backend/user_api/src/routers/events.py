efa4f6182f4836da0ac234ed1a2ce479458cd2e0
Rodion <ergerplus@gmail.com>
2026-04-08 16:50:50 +0200

    update

--- backend/user_api/src/routers/events.py
+++ backend/user_api/src/routers/events.py
@@ -18,8 +18,8 @@ async def event_streamer(tg_user_id: int):
     while True:
         try:
             event = await event_deque.get()
-            log.info(event)
             if event["tg_user_id"] == tg_user_id or event["tg_user_id"] == "all":
+                log.info(f"User: {event["tg_user_id"]} принял данные")
                 yield f"data: { json.dumps(event) }\n\n"
         except asyncio.TimeoutError as e:
             log.error(f"EventSender: {e}")
