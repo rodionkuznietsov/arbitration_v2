7f9cbc2b203a7d8de49b57e34f8fa78ca5205d5c
Rodion <ergerplus@gmail.com>
2026-04-08 16:20:01 +0200

    update

--- backend/user_api/main.py
+++ backend/user_api/main.py
@@ -24,8 +24,8 @@ from src.routers import user_router, events_router
 app = FastAPI(
     title="Arbitrage-Bot Api",
     version="Beta 0.0.5",
-    openapi_url="/api/openapi.json",
-    docs_url='/api/docs'
+    openapi_url="/openapi.json",
+    docs_url='/docs'
 )
 
 app.add_middleware(
