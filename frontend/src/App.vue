ea707cc5fc21d6e51eecd33eedf8b355796ddb5b
Rodion <ergerplus@gmail.com>
2026-04-10 00:19:14 +0200

    connect to rw

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -148,14 +148,14 @@
             }
           } else if (event_data.type == "websocket") {
             if (event_data.ws_data.channel == "order_book") {
-              orderBookStore.longLastPrice = event_data.result.data.order_book.long.last_price
-              orderBookStore.shortLastPrice = event_data.result.data.order_book.short.last_price
+              orderBookStore.longLastPrice = event_data.ws_data.result.data.order_book.long.last_price
+              orderBookStore.shortLastPrice = event_data.ws_data.result.data.order_book.short.last_price
               
-              orderBookStore.longAsks = event_data.result.data.order_book.long.asks
-              orderBookStore.longBids = event_data.result.data.order_book.long.bids
+              orderBookStore.longAsks = event_data.ws_data.result.data.order_book.long.asks
+              orderBookStore.longBids = event_data.ws_data.result.data.order_book.long.bids
               
-              orderBookStore.shortAsks = event_data.result.data.order_book.short.asks
-              orderBookStore.shortBids = event_data.result.data.order_book.short.bids
+              orderBookStore.shortAsks = event_data.ws_data.result.data.order_book.short.asks
+              orderBookStore.shortBids = event_data.ws_data.result.data.order_book.short.bids
             }
           } else if (event_data.type == "user_state") {
             userStateStore.changeStatus(event_data.payload.status)
