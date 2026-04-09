8313ccfada629d21e0b97f34911dafdb350237a1
Rodion <ergerplus@gmail.com>
2026-04-09 05:59:51 +0200

    connect to rw

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -145,8 +145,12 @@ import { useOrderBookStore } from './stores/orderbook';
             if (event_data.channel == "order_book") {
               orderBookStore.longLastPrice = event_data.result.data.order_book.long.last_price
               orderBookStore.shortLastPrice = event_data.result.data.order_book.short.last_price
+              
               orderBookStore.longAsks = event_data.result.data.order_book.long.asks
+              orderBookStore.longBids = event_data.result.data.order_book.long.bids
+              
               orderBookStore.shortAsks = event_data.result.data.order_book.short.asks
+              orderBookStore.shortBids = event_data.result.data.order_book.short.bids
             }
           }
         }
