416495b692e7cd54d76b61fb97630ded510580c4
Rodion <ergerplus@gmail.com>
2026-04-09 17:04:57 +0200

    connect to rw

--- backend/user_api/src/cache.py
+++ backend/user_api/src/cache.py
@@ -2,7 +2,7 @@ import asyncio
 from typing import Literal
 import structlog
 
-from .schemas import ErrorToUser
+from .schemas import MessageData
 
 log: structlog.PrintLogger = structlog.get_logger()
 
@@ -10,22 +10,22 @@ subscribes = {}
 user_state = {}
 
 async def push_to_subscribes(
-    event_data, 
-    error_message: ErrorToUser
+    message: MessageData
 ):
     try:
-        if error_message is not None:
-            for queue in subscribes[tg_user_id]:
-                try:
-                    queue.put_nowait(event_data)
-                except asyncio.QueueFull:
-                    pass  
-        else:
-            for queues in subscribes.values():
-                for queue in queues:
-                    try:
-                        queue.put_nowait(event_data)
-                    except asyncio.QueueFull:
-                        pass
+        pass
+        # if error_message is not None:
+        #     for queue in subscribes[tg_user_id]:
+        #         try:
+        #             queue.put_nowait(event_data)
+        #         except asyncio.QueueFull:
+        #             pass  
+        # else:
+            # for queues in subscribes.values():
+            #     for queue in queues:
+            #         try:
+            #             queue.put_nowait(event_data)
+            #         except asyncio.QueueFull:
+            #             pass
     except Exception as e:
         log.error(f"Cache: {e}")
