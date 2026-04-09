0bfda8c77942d55e88055bf87be6401cbdd86f6e
Rodion <ergerplus@gmail.com>
2026-04-09 07:19:41 +0200

    connect to rw

--- backend/user_api/src/routers/log.py
+++ backend/user_api/src/routers/log.py
@@ -91,6 +91,7 @@ async def add_log(data: UserLogSchema, token: Annotated[str, Depends(oauth2_sche
 
                 if user_state[tg_user_id]["devices"] == 0:
                     user_state.pop(tg_user_id, None)
+                    log.info(f"LogRouter -> Было удаленно состояние пользователя: {tg_user_id}")
                 log.info(f"Для клиента: {tg_user_id}, был отключен RustWebsocket")
 
     # Пушим новое собитие на все устройства
