38fd61ca5ef3c59f7639c6c19a7743688fae309a
Rodion <ergerplus@gmail.com>
2026-04-10 00:17:06 +0200

    connect to rw

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -147,7 +147,7 @@
               })
             }
           } else if (event_data.type == "websocket") {
-            if (event_data.channel == "order_book") {
+            if (event_data.ws_data.channel == "order_book") {
               orderBookStore.longLastPrice = event_data.result.data.order_book.long.last_price
               orderBookStore.shortLastPrice = event_data.result.data.order_book.short.last_price
               
