afbad77bd8b4665a6290267ca6cc237668033083
Rodion <ergerplus@gmail.com>
2026-04-09 06:52:34 +0200

    connect to rw

--- backend/user_api/src/cache.py
+++ backend/user_api/src/cache.py
@@ -1,19 +1,25 @@
 import asyncio
+import structlog
+
+log: structlog.PrintLogger = structlog.get_logger()
 
 subscribes = {}
 user_state = {}
 
 async def push_to_subscribes(event_data, tg_user_id: None):
-    if tg_user_id is not None:
-        for queue in subscribes[tg_user_id]:
-            try:
-                queue.put_nowait(event_data)
-            except asyncio.QueueFull:
-                pass  
-    else:
-        for queues in subscribes.values():
-            for queue in queues:
+    try:
+        if tg_user_id is not None:
+            for queue in subscribes[tg_user_id]:
                 try:
                     queue.put_nowait(event_data)
                 except asyncio.QueueFull:
-                    pass
+                    pass  
+        else:
+            for queues in subscribes.values():
+                for queue in queues:
+                    try:
+                        queue.put_nowait(event_data)
+                    except asyncio.QueueFull:
+                        pass
+    except Exception as e:
+        log.error(f"Cache: {e}")
