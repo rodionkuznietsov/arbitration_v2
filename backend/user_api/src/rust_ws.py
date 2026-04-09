a6b908c9632c4f3ecf8496d17503c74f4e4bbea9
Rodion <ergerplus@gmail.com>
2026-04-09 03:27:19 +0200

    connect to rw

--- backend/user_api/src/rust_ws.py
+++ backend/user_api/src/rust_ws.py
@@ -1,3 +1,5 @@
+import json
+
 import websockets
 import os
 from dotenv import load_dotenv
@@ -10,12 +12,12 @@ WEBSOCKET_URL = os.getenv("WEBSOCKET_URL")
 
 async def run_ws():
     async with websockets.connect(WEBSOCKET_URL) as websocket:
-        await websocket.send({
-            "action": "subscirbe",
+        await websocket.send(json.dumps({
+            "action": "subscribe",
             "channel": "order_book",
             "long_exchange": "Bybit",
             "short_exchange": "Gate.io",
             "symbol": "BTC"
-        })
+        }))
         response = await websocket.recv()
         log.info(response)
