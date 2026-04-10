344589efdc2bbd9b1b1ecdf619b41a46c6dfbf6c
Rodion <ergerplus@gmail.com>
2026-04-10 23:40:44 +0200

    connect to rw

--- backend/user_api/src/schemas/exchange.py
+++ backend/user_api/src/schemas/exchange.py
@@ -2,7 +2,7 @@ from enum import Enum
 from typing import Optional
 from pydantic import BaseModel, Field
 
-from ..schemas import EventDataTypeEnum
+from .event import EventDataTypeEnum
 
 class ExchangeEnum(str, Enum):
     Bybit = "bybit"
