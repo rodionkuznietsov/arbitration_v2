b9e47d956adc1bd0a7a3ab19946806a4962630f0
Rodion <ergerplus@gmail.com>
2026-04-10 23:16:23 +0200

    connect to rw

--- backend/user_api/src/routers/exchange.py
+++ backend/user_api/src/routers/exchange.py
@@ -26,28 +26,17 @@ async def get_available_exchanges():
 @router.post("/add_exchange", response_model=ResultSchema, tags=["exchanges"])
 async def add_exchange(exchange_data: ExchangeSchema):
     global exchange_cache
-    added = await database.add_exchange(exchange_data.name, exchange_data.is_available)
+    added = await database.add_exchange(exchange_data.name.lower(), exchange_data.is_available)
 
     if not added:
         raise HTTPException(status_code=400, detail=f"Биржа {exchange_data.name} уже существует в базе данных.")
 
-    # event_data = {
-    #     "type": EventDataTypeEnum.Exchange,
-    #     "tg_user_id": "all", 
-    #     "timestamp": time.time(),
-    #     "payload": {
-    #         "event": "add_exchange", 
-    #         "exchange_name": exchange_data.name,
-    #         "is_available": exchange_data.is_available
-    #     }
-    # }
-    
     message = MessageData(
         event_data=MessageEventData(
             type=EventDataTypeEnum.Exchange,
             payload=ExchangePayload(
                 event=ExchangeEvent.AddExchange,
-                exchange_name=exchange_data.name,
+                exchange_name=exchange_data.name.lower(),
                 is_available=exchange_data.is_available
             ),
             timestamp=int(time.time())
@@ -65,7 +54,7 @@ async def add_exchange(exchange_data: ExchangeSchema):
 @router.put("/update_exchange_availability", response_model=ResultSchema, tags=["exchanges"])
 async def update_exchange_availability(exchange_data: ExchangeSchema):
     global exchange_cache
-    updated = await database.update_exchange_availability(exchange_data.name, exchange_data.is_available)
+    updated = await database.update_exchange_availability(exchange_data.name.lower(), exchange_data.is_available)
     if not updated:
         return ResultSchema(
             status_code=404,
@@ -78,7 +67,7 @@ async def update_exchange_availability(exchange_data: ExchangeSchema):
             type=EventDataTypeEnum.Exchange,
             payload=ExchangePayload(
                 event=ExchangeEvent.UpdateExchange,
-                exchange_name=exchange_data.name,
+                exchange_name=exchange_data.name.lower(),
                 is_available=exchange_data.is_available
             ),
             timestamp=int(time.time())
