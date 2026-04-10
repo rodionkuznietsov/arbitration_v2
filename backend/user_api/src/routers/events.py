dfc385b84bfbb631f5a9a2f18c061d15515bd650
Rodion <ergerplus@gmail.com>
2026-04-10 17:48:30 +0200

    connect to rw

--- backend/user_api/src/routers/events.py
+++ backend/user_api/src/routers/events.py
@@ -23,13 +23,14 @@ async def event_streamer(data: asyncio.Queue, tg_user_id):
         log.error(f"EventsRouter -> {e}")
 
     finally:
-        # убираем только свою очередь
-        if tg_user_id in subscribes and data in subscribes[tg_user_id]:
-            subscribes[tg_user_id].remove(data)
-            log.info(
-                f"EventsRouter -> {tg_user_id} вышел из потока. "
-                f"Его текущие потоки: {len(subscribes.get(tg_user_id, []))}"
-            )
+        log.info(subscribes[tg_user_id])
+        # # убираем только свою очередь
+        # if tg_user_id in subscribes and data in subscribes[tg_user_id]:
+        #     subscribes[tg_user_id].remove(data)
+        #     log.info(
+        #         f"EventsRouter -> {tg_user_id} вышел из потока. "
+        #         f"Его текущие потоки: {len(subscribes.get(tg_user_id, []))}"
+        #     )
 
 @router.get("/subscribe/events/{tg_user_id}", tags=["events"])
 async def subscribe_events(tg_user_id: int):
