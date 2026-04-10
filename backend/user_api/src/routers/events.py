bfe81594957c620bafc01ae222a19ccfc6b5b62b
Rodion <ergerplus@gmail.com>
2026-04-10 05:47:36 +0200

    connect to rw

--- backend/user_api/src/routers/events.py
+++ backend/user_api/src/routers/events.py
@@ -41,7 +41,7 @@ async def subscribe_events(tg_user_id: int):
         subscribes[tg_user_id]["error_queue"].append(error_queue)
         
         if tg_user_id in user_state.exists_users():
-            push_to_subscribes(user_state[tg_user_id])
+            push_to_subscribes(user_state.get(tg_user_id))
 
         return StreamingResponse(
             event_streamer(success_queue, tg_user_id), 
