444d3618662b3ff1d80173c10e9d2b0caf71d1c9
Rodion <ergerplus@gmail.com>
2026-04-10 06:21:42 +0200

    connect to rw

--- backend/user_api/src/services/user_state.py
+++ backend/user_api/src/services/user_state.py
@@ -47,7 +47,7 @@ class UserState:
         
             raise UserStateError(status_code=404, message=f"Не удалось найти пользователя с id: {tg_user_id}")
         except UserStateError as e:
-            log.error(f"UserState -> {e}")
+            log.error(f"UserState(get) -> {e}")
     
     def isBotRunning(
         self,
