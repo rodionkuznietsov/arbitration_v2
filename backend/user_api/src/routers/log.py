5345646bd1ca0caf3d39bcfaf47fa41491500da2
Rodion <ergerplus@gmail.com>
2026-04-10 04:29:33 +0200

    connect to rw

--- backend/user_api/src/routers/log.py
+++ backend/user_api/src/routers/log.py
@@ -147,7 +147,7 @@ async def clear_all_logs():
 async def get_logs(token: Annotated[str, Depends(oauth2_scheme)]):
     tg_user_id = int(authothicate(token))
     
-    logs = []
+    last_logs = []
 
     if tg_user_id in user_state and len(user_state[tg_user_id].event_data.payload.logs) == 0:
         logs = await database.get_user_logs(tg_user_id)
@@ -159,12 +159,13 @@ async def get_logs(token: Annotated[str, Depends(oauth2_scheme)]):
             )
 
         user_state[tg_user_id].event_data.payload.logs = logs
+        last_logs = user_state[tg_user_id].event_data.payload.logs
 
     return ResultSchema(
         status_code=200,
         success=True,
         message=LogMessageSchema(
-            logs=logs
+            logs=last_logs
         )
     )
 
