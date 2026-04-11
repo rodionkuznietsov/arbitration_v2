4bc8ac440d072bb5f3ceb3a183eda3456562ecae
Rodion <ergerplus@gmail.com>
2026-04-11 23:12:15 +0200

    update user_api

--- backend/user_api/src/cache/exchange.py
+++ backend/user_api/src/cache/exchange.py
@@ -7,7 +7,7 @@ log: structlog.PrintLogger = structlog.get_logger()
 
 class ExchangeCache():
     def __init__(self, database: AsyncDatabase):
-        self.__available_exchanges__ = {}
+        self.__available_exchanges__ = []
         self.__database__ = database
 
     def exchange_mapper(
@@ -15,10 +15,12 @@ class ExchangeCache():
         raw_exchanges: list
     ):
         try:
-            mapped = {}
+            mapped = []
             for raw_exchange in raw_exchanges:
                 name = raw_exchange["name"].lower()
-                mapped[name] = raw_exchange["is_available"]
+                
+                if name not in mapped:
+                    mapped.append(name)
 
             return mapped
         except Exception as e:
@@ -38,7 +40,7 @@ class ExchangeCache():
             log.error(f"{{ exchange_service.get_available_exchanges_service }} -> {e}")
     
         return self.__available_exchanges__
-        
+
     def get_size(self):
         return len(self.__available_exchanges__)
 
@@ -54,11 +56,11 @@ class ExchangeCache():
     ):
         try:
             exchange_name: str = exchange_data.name.lower()
-            if exchange_data.is_available:
-                self.__available_exchanges__[exchange_name] = exchange_data.is_available
+            if exchange_data.is_available and exchange_name not in self.__available_exchanges__:
+                self.__available_exchanges__.append(exchange_name)
             else: 
                 if exchange_name in self.__available_exchanges__:
-                    self.__available_exchanges__.pop(exchange_name)        
+                    self.__available_exchanges__.remove(exchange_name)        
         except Exception as e:
             log.error(f"{{ exchange_cache.update_available_exchanges_in_cache }} -> {e}")
 
