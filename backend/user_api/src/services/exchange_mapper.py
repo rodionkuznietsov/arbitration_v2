0d61b87f0902b00abacea3ab737b9cb9d8dc2b98
Rodion <ergerplus@gmail.com>
2026-04-11 21:39:48 +0200

    update user_api

--- backend/user_api/src/services/exchange_mapper.py
+++ backend/user_api/src/services/exchange_mapper.py
@@ -1,9 +1,15 @@
+import structlog
+
+log: structlog.PrintLogger = structlog.get_logger()
+
 def exchange_mapper(
     raw_exchanges: list
 ):
-    mapped = {}
+    try:
+        mapped = {}
+        for raw_exchange in raw_exchanges:
+            mapped[raw_exchange["name"]] = raw_exchange["is_available"]
 
-    for raw_exchange in raw_exchanges:
-        mapped[raw_exchange["name"]] = raw_exchange["is_available"]
-
-    return mapped
+        return mapped
+    except Exception as e:
+        log.error(f"{{ exchange_mapper }}  -> {e}")
