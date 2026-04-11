07e2e1dd8f45970361ae33883f3f353cff841a04
Rodion <ergerplus@gmail.com>
2026-04-11 16:26:22 +0200

    connect to rw

--- frontend/src/handlers/user_state.handler.js
+++ frontend/src/handlers/user_state.handler.js
@@ -14,10 +14,10 @@ export function user_state_handler(
     }
 
     userStateStore.symbol = event_data.payload.symbol
-    userStateStore.longExchange = event_data.payload.longExchange
+    userStateStore.longExchange = event_data.payload.longExchange == "unknown" ? event_data.payload.longExchange : "Нет доступной биржи"
     userStateStore.longOrderType = event_data.payload.longOrderType 
 
-    userStateStore.shortExchange = event_data.payload.shortExchange
+    userStateStore.shortExchange = event_data.payload.shortExchange == "unknown" ? event_data.payload.shortExchange : "Нет доступной биржи"
     userStateStore.shortOrderType = event_data.payload.shortOrderType
 
 }
