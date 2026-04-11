42a4877c479aedc2b4546ddf26765b56b73d0505
Rodion <ergerplus@gmail.com>
2026-04-11 21:07:25 +0200

    update user_api

--- backend/user_api/src/routers/exchange.py
+++ backend/user_api/src/routers/exchange.py
@@ -73,6 +73,8 @@ async def update_exchange_availability(exchange_data: ExchangeSchema):
     ):
         available_exchanges.append(exchange_data.name.lower())
 
+    log.info(available_exchanges)
+
     # Меняем данные для userState, чтобы навсякий случай избежать проблему с рассихроностью
     try:
         if not exchange_data.is_available:
