3229650e68ffa2e3b9a4eb4b72ed947dc070ffd1
Rodion <ergerplus@gmail.com>
2026-04-12 16:18:50 +0200

    update user_api 12 apr

--- frontend/src/utils/send_cmd.js
+++ frontend/src/utils/send_cmd.js
@@ -1,4 +1,4 @@
-import { API_URL } from "@/config";
+// import { API_URL } from "@/config";
 
 export async function send_cmd(
     token,
@@ -8,25 +8,31 @@ export async function send_cmd(
 ) {
     if (event.toLowerCase() == "exchange_update") {
         try {
-            const response = await fetch(`${API_URL}/user/state/update`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                },
-                body: JSON.stringify({ 
-                    event: event, 
-                    data: {
-                        exchange_name: value.toLowerCase(),
-                        market_type: market_type.toLowerCase()
-                    } 
-                })
-            })
+            alert(JSON.stringify({
+                token,
+                event,
+                market_type,
+                value
+            }))
+            // const response = await fetch(`${API_URL}/user/state/update`, {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //         'Authorization': `Bearer ${token}`
+            //     },
+            //     body: JSON.stringify({ 
+            //         event: event, 
+            //         data: {
+            //             exchange_name: value.toLowerCase(),
+            //             market_type: market_type.toLowerCase()
+            //         } 
+            //     })
+            // })
 
-            if (!response.ok) {
-                const errorData = await response.json()
-                alert(JSON.stringify(errorData))
-            }
+            // if (!response.ok) {
+            //     const errorData = await response.json()
+            //     alert(JSON.stringify(errorData))
+            // }
         } catch(err) {
             alert(JSON.stringify(err))
         }
