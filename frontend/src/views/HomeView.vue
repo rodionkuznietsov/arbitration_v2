8e4e6f0cd46488143d03a8e17781c5e7d10dfa49
Rodion <ergerplus@gmail.com>
2026-04-12 16:32:47 +0200

    update user_api 12 apr

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -72,7 +72,7 @@
                 <label for="order" id="form_label">Лонг:</label>
                 <img src="../assets/icons/up.svg" alt="" draggable="false">
               </div>
-              <FormCombobox v-model="userStore.longExchange" :values="configStore.exchanges" :event="'exchange_update'" :market_type="long"/>
+              <FormCombobox v-model="userStore.longExchange" :values="configStore.exchanges" :event="'exchange_update'" :market_type="'long'"/>
             </div>
 
             <div class="form-group">
@@ -96,7 +96,7 @@
                 <label for="order" id="form_label">Шорт:</label>
                 <img class="img_reverse" src="../assets/icons/up.svg" alt="" draggable="false">
               </div>
-              <FormCombobox v-model="userStore.shortExchange" :values="configStore.exchanges" :event="'exchange_update'" :market_type="long"/>
+              <FormCombobox v-model="userStore.shortExchange" :values="configStore.exchanges" :event="'exchange_update'" :market_type="'short'"/>
             </div>
 
             <div class="form-group">
