c29bc1670a7566a3a2561aac3934f720137a8389
Rodion <ergerplus@gmail.com>
2026-04-12 02:10:55 +0200

    update user_api

--- frontend/src/handlers/user_state.handler.js
+++ frontend/src/handlers/user_state.handler.js
@@ -4,20 +4,26 @@ export function user_state_handler(
     orderBookStore,
     configStore
 ) {
-    if (userStateStore.isBotRunning) {
-        orderBookStore.updateHeader(
-            event_data.payload.symbol,
-            event_data.payload.longExchange,
-            event_data.payload.longOrderType,
-            event_data.payload.shortExchange,
-            event_data.payload.shortOrderType
-        )
-    }
+    event_data
+    userStateStore
+    orderBookStore
+    configStore
+    alert(event_data.payload)
+    
+    // if (userStateStore.isBotRunning) {
+    //     orderBookStore.updateHeader(
+    //         event_data.payload.symbol,
+    //         event_data.payload.longExchange,
+    //         event_data.payload.longOrderType,
+    //         event_data.payload.shortExchange,
+    //         event_data.payload.shortOrderType
+    //     )
+    // }
 
-    userStateStore.symbol = event_data.payload.symbol
-    userStateStore.longExchange = event_data.payload.longExchange != "unknown" ? event_data.payload.longExchange : configStore.exchanges[0] ? configStore.exchanges[0] : "Нет доступной биржи"
-    userStateStore.longOrderType = event_data.payload.longOrderType 
+    // userStateStore.symbol = event_data.payload.symbol
+    // userStateStore.longExchange = event_data.payload.longExchange != "unknown" ? event_data.payload.longExchange : configStore.exchanges[0] ? configStore.exchanges[0] : "Нет доступной биржи"
+    // userStateStore.longOrderType = event_data.payload.longOrderType 
 
-    userStateStore.shortExchange = event_data.payload.shortExchange != "unknown" ? event_data.payload.shortExchange : configStore.exchanges[1] ? configStore.exchanges[1] : userStateStore.longExchange
-    userStateStore.shortOrderType = event_data.payload.shortOrderType
+    // userStateStore.shortExchange = event_data.payload.shortExchange != "unknown" ? event_data.payload.shortExchange : configStore.exchanges[1] ? configStore.exchanges[1] : userStateStore.longExchange
+    // userStateStore.shortOrderType = event_data.payload.shortOrderType
 }
