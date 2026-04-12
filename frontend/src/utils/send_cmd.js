5994f562345b96c5cbbfe8dfa74e43b0ef53e65d
Rodion <ergerplus@gmail.com>
2026-04-12 15:55:41 +0200

    update user_api 12 apr

--- frontend/src/utils/send_cmd.js
+++ frontend/src/utils/send_cmd.js
@@ -1,5 +1,7 @@
 // import { API_URL } from "@/config";
 
+import { API_URL } from "@/config"
+
 export async function send_cmd(
     token,
     event,
@@ -9,15 +11,19 @@ export async function send_cmd(
     data
     
     try {
-        alert(event)
-    //     await fetch(`${API_URL}/user/state/update`, {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //             'Authorization': `Bearer ${token}`
-    //         },
-    //         body: JSON.stringify({ event, data })
-    //     })
+        const response = await fetch(`${API_URL}/user/state/update`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({ event, data })
+        })
+
+        if (!response.ok) {
+            const errorData = await response.json()
+            alert(JSON.stringify(errorData))
+        }
     } catch(err) {
         console.log(err)
     }
