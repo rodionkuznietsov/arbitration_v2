3fd2749d5eb8e81528610255ba7990fa077188aa
Rodion <ergerplus@gmail.com>
2026-04-13 20:28:33 +0200

    fix  13 apr

--- backend/user_api/src/services/user_state.py
+++ backend/user_api/src/services/user_state.py
@@ -117,9 +117,6 @@ class UserState:
     ):
         try:
             if tg_user_id in self.__user_state__:
-                if self.__is_bot_running__(tg_user_id):
-                    self.__user_state__[tg_user_id].event_data.payload.bot_config.draft = self.__user_state__[tg_user_id].event_data.payload.bot_config.active
-                    
                 return self.__user_state__[tg_user_id]
         
             raise UserStateError(status_code=404, message=f"Не удалось найти пользователя с id: {tg_user_id}")
