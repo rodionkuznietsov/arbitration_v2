ddc78dfd750d1f5486984d7865ada5a730e24cff
Rodion <ergerplus@gmail.com>
2026-04-13 19:00:04 +0200

    fix  13 apr

--- frontend/src/handlers/websocket.handler.js
+++ frontend/src/handlers/websocket.handler.js
@@ -2,14 +2,14 @@ export function ws_handler(
     event_data,
     orderBookStore
 ) { 
-    if (event_data.ws_data.channel == "order_book") {
-        orderBookStore.longLastPrice = event_data.ws_data.result.data.order_book.long.last_price
-        orderBookStore.shortLastPrice = event_data.ws_data.result.data.order_book.short.last_price
+    if (event_data.payload.channel == "order_book") {
+        orderBookStore.longLastPrice = event_data.payload.result.data.order_book.long.last_price
+        orderBookStore.shortLastPrice = event_data.payload.result.data.order_book.short.last_price
         
-        orderBookStore.longAsks = event_data.ws_data.result.data.order_book.long.asks
-        orderBookStore.longBids = event_data.ws_data.result.data.order_book.long.bids
+        orderBookStore.longAsks = event_data.payload.result.data.order_book.long.asks
+        orderBookStore.longBids = event_data.payload.result.data.order_book.long.bids
         
-        orderBookStore.shortAsks = event_data.ws_data.result.data.order_book.short.asks
-        orderBookStore.shortBids = event_data.ws_data.result.data.order_book.short.bids
+        orderBookStore.shortAsks = event_data.payload.result.data.order_book.short.asks
+        orderBookStore.shortBids = event_data.payload.result.data.order_book.short.bids
     }
 }
