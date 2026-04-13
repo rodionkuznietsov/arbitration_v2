30eea654644fdae8c00f95ab9b6d0bf1f55174ad
Rodion <ergerplus@gmail.com>
2026-04-13 20:06:24 +0200

    fix  13 apr

--- backend/user_api/src/routers/user.py
+++ backend/user_api/src/routers/user.py
@@ -22,19 +22,19 @@ async def update_user_state(
     
     tg_user_id = int(authothicate(token))
 
-    try:
-        if data.event == UserStateEventTypeEnum.ExchangeUpdate:
-            user_state.update_draft_exchange(tg_user_id, data.data.exchange_name, data.data.market_type)
-            log.info(f"{{ user_router.update_user_state.update_exchange }}")
-    except Exception as e:
-        log.error(f"{{ user_router.update_user_state.update_exchange }} -> {e}")
+    # try:
+    #     if data.event == UserStateEventTypeEnum.ExchangeUpdate:
+    #         user_state.update_draft_exchange(tg_user_id, data.data.exchange_name, data.data.market_type)
+    #         log.info(f"{{ user_router.update_user_state.update_exchange }}")
+    # except Exception as e:
+    #     log.error(f"{{ user_router.update_user_state.update_exchange }} -> {e}")
 
-    try:
-        if data.event == UserStateEventTypeEnum.SymbolUpdate:
-            user_state.update_draft_symbol(tg_user_id, data.data.symbol)
-            log.info(f"{{ user_router.update_user_state.update_exchange }}")
-    except Exception as e:
-        log.error(f"{{ user_router.update_user_state.update_exchange }} -> {e}")
+    # try:
+    #     if data.event == UserStateEventTypeEnum.SymbolUpdate:
+    #         user_state.update_draft_symbol(tg_user_id, data.data.symbol)
+    #         log.info(f"{{ user_router.update_user_state.update_exchange }}")
+    # except Exception as e:
+    #     log.error(f"{{ user_router.update_user_state.update_exchange }} -> {e}")
 
     return ResultSchema(
         status_code=200,
