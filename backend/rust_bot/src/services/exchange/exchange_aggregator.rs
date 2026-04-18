a85f63d58b81024ee154cdd722f0b175efd44378
Rodion <ergerplus@gmail.com>
2026-04-18 08:59:04 +0200

    fix 18 apr

--- backend/rust_bot/src/services/exchange/exchange_aggregator.rs
+++ backend/rust_bot/src/services/exchange/exchange_aggregator.rs
@@ -135,18 +135,26 @@ impl ExchangeStore {
                             symbol, 
                             snapshot  
                         } => {
+                            if symbol == "btcusdt" {
+                                for (price, volume) in snapshot.a.iter().rev() {
+                                    let price_without_tick = *price as f64 / PRICE_TICK; 
+
+                                    tracing::info!("ExchangeAggregator: {} -> {}", price_without_tick, volume)
+                                }
+                            }
+                            
                             if let Some(data) = self.market_data.get_mut(&symbol) {
                                 data.snapshot = Some(snapshot);
 
-                                if symbol == "btcusdt" {
-                                    if let Some(sn) = &data.snapshot {
-                                        for (price, volume) in sn.a.iter().rev() {
-                                            let price_without_tick = *price as f64 / PRICE_TICK; 
+                                // if symbol == "btcusdt" {
+                                //     if let Some(sn) = &data.snapshot {
+                                //         for (price, volume) in sn.a.iter().rev() {
+                                //             let price_without_tick = *price as f64 / PRICE_TICK; 
 
-                                            tracing::info!("ExchangeAggregator: {} -> {}", price_without_tick, volume)
-                                        }
-                                    }
-                                }
+                                //             tracing::info!("ExchangeAggregator: {} -> {}", price_without_tick, volume)
+                                //         }
+                                //     }
+                                // }
                             }
                         }
                         BookEvent::Delta { 
