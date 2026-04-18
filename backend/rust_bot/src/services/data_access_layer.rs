53b706a123f67a0eaa684f10ab42bdccdc29503c
Rodion <ergerplus@gmail.com>
2026-04-18 16:05:06 +0200

    fix 18 aprA

--- backend/rust_bot/src/services/data_access_layer.rs
+++ backend/rust_bot/src/services/data_access_layer.rs
@@ -92,19 +92,16 @@ impl DataAccessLayer {
 
                                     while watch_aggregator_tx.changed().await.is_ok() {
                                         let (symbol, exchange_data) = watch_aggregator_tx.borrow().clone();
-
-                                        
-                                            tracing::info!("{symbol}");
-                                            // if let Some(e) = data_aggregator_tx.send_timeout(
-                                            //     DataAggregatorCmd::UpdateData { 
-                                            //         exchange_id, 
-                                            //         symbol,
-                                            //         data: exchange_data
-                                            //     }, 
-                                            // Duration::from_millis(10)
-                                            // ).await.err() {
-                                            //     tracing::error!("DataAccessLayer(FromExchangeAggregator) -> {e};")
-                                            // }
+                                        if let Some(e) = data_aggregator_tx.send_timeout(
+                                            DataAggregatorCmd::UpdateData { 
+                                                exchange_id, 
+                                                symbol,
+                                                data: exchange_data
+                                            }, 
+                                        Duration::from_millis(10)
+                                        ).await.err() {
+                                            tracing::error!("DataAccessLayer(FromExchangeAggregator) -> {e};")
+                                        }
                                     }
                                 }
                             });
