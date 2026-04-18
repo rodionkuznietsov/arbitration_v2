95692d10c15594822e688be80aa3f3bc9ecda81a
Rodion <ergerplus@gmail.com>
2026-04-18 13:13:09 +0200

    fix 18 aprA

--- backend/rust_bot/src/services/exchange/exchange_aggregator.rs
+++ backend/rust_bot/src/services/exchange/exchange_aggregator.rs
@@ -116,7 +116,11 @@ impl ExchangeStore {
         let mut last_version_id = 0;
         while let Ok(_) = self.rx.changed().await {
             let cmd = self.rx.borrow();
-            match *cmd {
+
+            match &*cmd {
+                ExchangeStoreCMD::Default => {
+
+                }
                 ExchangeStoreCMD::RegisterSymbol { 
                     symbol 
                 } => {
@@ -139,106 +143,107 @@ impl ExchangeStore {
                             symbol, 
                             snapshot  
                         } => {
-                            if let Some(data) = self.market_data.get_mut(&symbol) {
-                                data.snapshot = Some(snapshot);
-                                let _ = self.watch_tx.send((Arc::new(symbol), Arc::new(data.to_owned())));
+                            if let Some(data) = self.market_data.get_mut(&*symbol) {
+                                data.snapshot = Some(snapshot.clone());
+                                let _ = self.watch_tx.send((Arc::new(symbol.clone()), Arc::new(data.to_owned())));
                             }
                         }
-                        BookEvent::Delta { 
-                            symbol, 
-                            delta 
-                        } => {
-                            if let Some(data) = self.market_data.get_mut(&symbol) {
-                                if let Some(snapshot) = &mut data.snapshot {
-                                    match snapshot.last_update_id {
-                                        Some(_) => {
-                                            let from_version = delta.from_version.unwrap();
-                                            let to_version = delta.to_version.unwrap();
+                //         BookEvent::Delta { 
+                //             symbol, 
+                //             delta 
+                //         } => {
+                //             if let Some(data) = self.market_data.get_mut(&symbol) {
+                //                 if let Some(snapshot) = &mut data.snapshot {
+                //                     match snapshot.last_update_id {
+                //                         Some(_) => {
+                //                             let from_version = delta.from_version.unwrap();
+                //                             let to_version = delta.to_version.unwrap();
 
-                                            for (price, volume) in delta.a {
-                                                if volume == 0.0 {
-                                                    snapshot.a.remove(&price);
-                                                } else {
-                                                    if let Some(v) = snapshot.a.get_mut(&price) {
-                                                        *v = volume;
-                                                    } else {
-                                                        snapshot.a.insert(price, volume);
-                                                    }
-                                                }
-                                            }
+                //                             for (price, volume) in delta.a {
+                //                                 if volume == 0.0 {
+                //                                     snapshot.a.remove(&price);
+                //                                 } else {
+                //                                     if let Some(v) = snapshot.a.get_mut(&price) {
+                //                                         *v = volume;
+                //                                     } else {
+                //                                         snapshot.a.insert(price, volume);
+                //                                     }
+                //                                 }
+                //                             }
 
-                                            for (price, volume) in delta.b {
-                                                if volume == 0.0 {
-                                                    snapshot.b.remove(&price);
-                                                } else {
-                                                    if let Some(v) = snapshot.b.get_mut(&price) {
-                                                        *v = volume;
-                                                    } else {
-                                                        snapshot.b.insert(price, volume);
-                                                    }
-                                                }
-                                            }
+                //                             for (price, volume) in delta.b {
+                //                                 if volume == 0.0 {
+                //                                     snapshot.b.remove(&price);
+                //                                 } else {
+                //                                     if let Some(v) = snapshot.b.get_mut(&price) {
+                //                                         *v = volume;
+                //                                     } else {
+                //                                         snapshot.b.insert(price, volume);
+                //                                     }
+                //                                 }
+                //                             }
 
-                                            if last_version_id == 0 {
-                                                continue;
-                                            }
+                //                             if last_version_id == 0 {
+                //                                 continue;
+                //                             }
 
-                                            if from_version != last_version_id {
-                                                println!("[OrderBookManager]: Packet loss detected");
-                                                continue;
-                                            }
-                                            last_version_id = to_version + 1;  
-                                            let _ = self.watch_tx.send((Arc::new(symbol), Arc::new(data.to_owned())));
-                                        }
-                                        None => {
-                                            for (price, volume) in delta.a {
-                                                if volume == 0.0 {
-                                                    snapshot.a.remove(&price);
-                                                } else {
-                                                    if let Some(v) = snapshot.a.get_mut(&price) {
-                                                        *v = volume;
-                                                    } else {
-                                                        snapshot.a.insert(price, volume);
-                                                    }
-                                                }
-                                            }
+                //                             if from_version != last_version_id {
+                //                                 println!("[OrderBookManager]: Packet loss detected");
+                //                                 continue;
+                //                             }
+                //                             last_version_id = to_version + 1;  
+                //                             let _ = self.watch_tx.send((Arc::new(symbol), Arc::new(data.to_owned())));
+                //                         }
+                //                         None => {
+                //                             for (price, volume) in delta.a {
+                //                                 if volume == 0.0 {
+                //                                     snapshot.a.remove(&price);
+                //                                 } else {
+                //                                     if let Some(v) = snapshot.a.get_mut(&price) {
+                //                                         *v = volume;
+                //                                     } else {
+                //                                         snapshot.a.insert(price, volume);
+                //                                     }
+                //                                 }
+                //                             }
 
-                                            for (price, volume) in delta.b {
-                                                if volume == 0.0 {
-                                                    snapshot.b.remove(&price);
-                                                } else {
-                                                    if let Some(v) = snapshot.b.get_mut(&price) {
-                                                        *v = volume;
-                                                    } else {
-                                                        snapshot.b.insert(price, volume);
-                                                    }
-                                                }
-                                            }
-                                            let _ = self.watch_tx.send((Arc::new(symbol), Arc::new(data.to_owned())));
-                                        }
-                                    } 
-                                }
-                            }
-                        },
-                        BookEvent::TickerUpdate { 
-                            symbol, 
-                            last_price, 
-                            volume 
-                        } => {
-                            if let Some(data) = self.market_data.get_mut(&symbol) {
-                                data.last_price = Some(last_price);
-                                data.volume24h = Some(volume);
-                                let _ = self.watch_tx.send((Arc::new(symbol), Arc::new(data.to_owned())));
-                            }
-                        },
+                //                             for (price, volume) in delta.b {
+                //                                 if volume == 0.0 {
+                //                                     snapshot.b.remove(&price);
+                //                                 } else {
+                //                                     if let Some(v) = snapshot.b.get_mut(&price) {
+                //                                         *v = volume;
+                //                                     } else {
+                //                                         snapshot.b.insert(price, volume);
+                //                                     }
+                //                                 }
+                //                             }
+                //                             let _ = self.watch_tx.send((Arc::new(symbol), Arc::new(data.to_owned())));
+                //                         }
+                //                     } 
+                //                 }
+                //             }
+                //         },
+                        // BookEvent::TickerUpdate { 
+                        //     symbol, 
+                        //     last_price, 
+                        //     volume 
+                        // } => {
+                        //     if let Some(data) = self.market_data.get_mut(&symbol) {
+                        //         data.last_price = Some(last_price);
+                        //         data.volume24h = Some(volume);
+                        //         let _ = self.watch_tx.send((Arc::new(symbol), Arc::new(data.to_owned())));
+                        //     }
+                        // },
+                        _ => {}
                     }
-                }
-                ExchangeStoreCMD::Subscribe { 
-                    reply
-                } => {             
-                    reply.send(self.watch_rx.clone()).ok();
                 },
-                ExchangeStoreCMD::Default => {}
+                // ExchangeStoreCMD::Subscribe { 
+                //     reply
+                // } => {             
+                //     reply.send(self.watch_rx.clone()).ok();
+                // },
+                _ => {}
             }
         }
     }
