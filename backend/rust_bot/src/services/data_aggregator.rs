fce0f37edfc386df1edcb2426dc3e8415774ca46
Rodion <ergerplus@gmail.com>
2026-04-18 15:04:24 +0200

    fix 18 aprA

--- backend/rust_bot/src/services/data_aggregator.rs
+++ backend/rust_bot/src/services/data_aggregator.rs
@@ -110,6 +110,8 @@ impl DataAggregator {
                 symbol,
                 mut data
             } => {
+                tracing::info!("{data:?}");
+
                 if let Some(exchanges) = self.markets.get_mut(&symbol) {
                     if let Some(old_data) = exchanges.get_mut(&exchange_id) {
                         let data_mut = Arc::make_mut(&mut data);
