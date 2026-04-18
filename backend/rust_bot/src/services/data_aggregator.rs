91bfbc94d4b8fa275016d6f4e91b12579f10dc85
Rodion <ergerplus@gmail.com>
2026-04-18 16:31:17 +0200

    fix 18 aprA

--- backend/rust_bot/src/services/data_aggregator.rs
+++ backend/rust_bot/src/services/data_aggregator.rs
@@ -111,7 +111,7 @@ impl DataAggregator {
                 mut data
             } => {
                 if symbol.to_string() == "btcusdt" {
-
+                    tracing::info!("{symbol}")
                 }
 
                 if let Some(exchanges) = self.markets.get_mut(&symbol) {
