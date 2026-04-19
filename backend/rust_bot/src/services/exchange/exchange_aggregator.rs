a70544c9f89299295b09d195ca5c9777c6d8f05c
Rodion <ergerplus@gmail.com>
2026-04-19 13:26:39 +0200

    fix 19 apr

--- backend/rust_bot/src/services/exchange/exchange_aggregator.rs
+++ backend/rust_bot/src/services/exchange/exchange_aggregator.rs
@@ -149,15 +149,10 @@ impl ExchangeStore {
                                     symbol, 
                                     snapshot  
                                 } => {
-                                    if self.id == ExchangeType::Bybit {
-                                        if symbol == "btcusdt" {
-                                            tracing::info!("{snapshot:?}")
-                                        }
+                                    if let Some(data) = self.market_data.get_mut(&*symbol) {
+                                        data.snapshot = Some(snapshot);
+                                        let _ = self.watch_tx.send((Arc::new(symbol.clone()), Arc::new(data.to_owned())));
                                     }
-                                    // if let Some(data) = self.market_data.get_mut(&*symbol) {
-                                    //     data.snapshot = Some(snapshot);
-                                    //     let _ = self.watch_tx.send((Arc::new(symbol.clone()), Arc::new(data.to_owned())));
-                                    // }
                                 },
                                 _ => {}
                             }
