6b91ccc22963dc73662488beb20f6ef1f440eb77
Rodion <ergerplus@gmail.com>
2026-04-21 16:38:21 +0200

    fix 21 apr

--- backend/rust_bot/src/services/data_mapping.rs
+++ backend/rust_bot/src/services/data_mapping.rs
@@ -158,6 +158,8 @@ impl DataMapping {
                                 let long_json_lines = self.snapshot_to_json(long_snapshot, &long_last_price);
                                 let short_json_lines = self.snapshot_to_json(short_snapshot, &short_last_price);
                                 
+                                tracing::info!("{:?}", long_arc);
+                            
                                 if let (
                                     Some(long), 
                                     Some(short)
@@ -168,7 +170,7 @@ impl DataMapping {
                                     let long_arc = Arc::new(long);
                                     let short_arc = Arc::new(short);
 
-                                    tracing::info!("{:?}", long_arc);
+                                    // tracing::info!("{:?}", long_arc);
                                     
                                     // self.send_message_with_key(
                                     //     ChannelType::OrderBook,
