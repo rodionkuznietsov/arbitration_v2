bcf6162b67fb799bd8d8d099ddc7592234b45c7f
Rodion <ergerplus@gmail.com>
2026-04-21 16:38:46 +0200

    fix 21 apr

--- backend/rust_bot/src/services/data_mapping.rs
+++ backend/rust_bot/src/services/data_mapping.rs
@@ -158,7 +158,7 @@ impl DataMapping {
                                 let long_json_lines = self.snapshot_to_json(long_snapshot, &long_last_price);
                                 let short_json_lines = self.snapshot_to_json(short_snapshot, &short_last_price);
                                 
-                                tracing::info!("{:?}", long_arc);
+                                tracing::info!("{:?}", long_json_lines);
                             
                                 if let (
                                     Some(long), 
